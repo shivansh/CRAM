516e97ea2b5cb62f2fc894d6bca902d9291b14b4
Shivansh Rai <shivanshrai84@gmail.com>
2016-10-01 23:47:52 +0530

    Basic MVC working

---
+++ superlists/lists/scrape_user.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import csv
+
+import urllib.request
+
+def scrape(roll):
+    url = "http://172.26.142.68/dccourse/studdc.php?roll_no=" + str(roll)
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    # page = urllib2.urlopen(url)
+    soup = BeautifulSoup(response.read(), "lxml")
+    # soup = BeautifulSoup(open('test.html'))
+
+    table = soup.find('table')
+
+    rows = []
+
+    for row in table.find_all('tr'):
+        rows.append([val.text.encode('utf8') for val in row.find_all('td', limit=4)])
+
+
+    # for nrow in rows:
+        # nrow.append(str(roll))
+
+    with open('output.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(row for row in rows if row)
+
+    # print(rows[1])
+
+    import pandas as pd
+    from collections import defaultdict
+
+    columns = defaultdict(list) # each value in each column is appended to a list
+
+    with open('output.csv') as f:
+        reader = csv.reader(f)
+        # reader.next()
+        for row in reader:
+            for (i,v) in enumerate(row):
+                columns[i].append(v[3:len(v)-2])
+
+    # print(columns[2])
+    return columns[2]
+
+# scrape(14658)
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item
+from lists.scrape_user import scrape
 
 def home_page(request):
     new_item_text = ''
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
+        course_list = scrape(new_item_text)
+        for course in course_list:
+            Item.objects.create(text=course)
+        # Item.objects.create(text=(scrape(new_item_text)))
+        # items = []
+        # items = scrape(new_item_text)
         return redirect('/')
 
     items = Item.objects.all()
