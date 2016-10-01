d6e5c37d06733ae0d51d9647fe12b2c656c87473
Shivansh Rai <shivanshrai84@gmail.com>
2016-10-01 12:20:56 +0530

    Front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -28,7 +28,7 @@ class NewTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
+                any(row.text == '1: Buy peacock feathers' for row in rows), "todo item did not appear"
                 )
         # self.fail('Finish the test')
 
