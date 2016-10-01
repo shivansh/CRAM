30e16ec63d3eb736531bd55104f70dc1e12528c2
Shivansh Rai <shivanshrai84@gmail.com>
2016-10-01 16:15:48 +0530

    Refactor and modularize the unit tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -37,12 +37,25 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+        # The following test is now no longer required since we now redirect
+        """
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
                 'home.html',
                 {'new_item_text': 'A new list item'}
                 )
         self.assertEqual(response.content.decode(), expected_html)
+        """
+
+    def test_redirect_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
+    new_item_text = ''
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        return redirect('/')
 
     return render(request, 'home.html', {
         'new_item_text': new_item_text
